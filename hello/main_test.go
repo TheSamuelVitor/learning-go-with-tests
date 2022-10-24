2606de0a7548f20e73262d7665a064a355f012e7
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-10-24 10:33:18 -0300

    feat: adding subtests

--- hello/main_test.go
+++ hello/main_test.go
@@ -4,11 +4,26 @@ import (
 	"testing"
 )
 
-func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}
+}
+
+func TestHelloPasses(t *testing.T) {
+	const englishHelloPrefix = "Hello, "
+	const name = "Samuel"
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello(name)
+		want := englishHelloPrefix + name
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("see what happens when name is empty", func(t *testing.T) {
+		got := Hello("")
+		want := englishHelloPrefix + "World"
+		assertCorrectMessage(t, got, want)
+	})
 }
