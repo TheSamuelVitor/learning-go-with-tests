c19ad2aac5c0fdc19c3c885a4b97dbe6e3ce9ec4
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-11-03 11:35:11 -0300

    :white_check_mark: test: adding new tests to the struct

--- 6 - pointers and erros/main_test.go
+++ 6 - pointers and erros/main_test.go
@@ -4,27 +4,42 @@ import (
 	"testing"
 )
 
-func TestWallet(t *testing.T) {
-	
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		
-		if got != want {
-			t.Errorf("\ngot %v\nwant %v", got, want)
-		}
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("\ngot %v\nwant %v", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
 	}
 
-	assertError := func(t testing.TB, got error, want string){
-		t.Helper()
-		if got == nil {
-			t.Fatal("didn't get error but wanted one")
-		}
+	if got != want {
+		t.Errorf("got %q\nwant %q", got, want)
+	}
+}
 
-		if got.Error() != want {
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func TestWallet(t *testing.T) {
+
+	t.Run("test Bitcoin wallet", func(t *testing.T) {
+		got := Bitcoin.String(20)
+		want := "20 BTC"
+		if got != want {
 			t.Errorf("got %q\nwant %q", got, want)
 		}
-	}
+	})
 
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
@@ -33,21 +48,21 @@ func TestWallet(t *testing.T) {
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
-
 		wallet := Wallet{
 			balance: Bitcoin(20),
 		}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdram insuficient funds", func(t *testing.T) {
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
-		wallet := Wallet{startingBalance}
-		err := wallet.Withdraw(Bitcoin(10))
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
 
-		assertError(t, err, "cannot withdraw, insuficient funds")
+		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
 	})
 }
