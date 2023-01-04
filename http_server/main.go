0146e6b9a9303e26fc785dc93ba19999f577a9d3
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2023-01-04 09:45:46 -0300

    adicionando novos testes e funcoes

--- http_server/main.go
+++ http_server/main.go
@@ -1,10 +1,11 @@
-package httpserver
+package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "20")	
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
--- http_server/main_test.go
+++ http_server/main_test.go
@@ -1,4 +1,4 @@
-package httpserver
+package main
 
 import (
 	"net/http"
@@ -7,14 +7,28 @@ import (
 )
 
 func TestGETPlayer(t *testing.T) {
-	t.Run("returns Pepper's score", func (t *testing.T)  {
-		request, _ := http.NewRequest(http.MethodGet, "players/Pepper", nil)
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
 		got := response.Body.String()
 		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
