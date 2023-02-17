33b899f948fb41fbe549071e9769578cb4450d8c
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2023-02-17 11:25:48 -0300

    :zap: perf: melhorando resposta de servidor usando select e channels

--- select/main.go
+++ select/main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
+var tenSecondTimeout = 10 * time.Second
 
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 
-	if aDuration > bDuration {
-		return a
+	/*
+		Select lets wait on multiple channel operations
+	*/
+
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out wainting for %s and %s", a, b)
 	}
+}
 
-	return b
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
